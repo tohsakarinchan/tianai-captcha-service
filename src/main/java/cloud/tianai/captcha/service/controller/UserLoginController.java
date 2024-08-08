efee5a1df7c7ef3729c8cb16fad1f6c679c2ae74
deywak <wjy_19875928547@163.com>
2024-08-08 15:23:34 +0800

    add login func

--- src/main/java/cloud/tianai/captcha/service/controller/UserLoginController.java
+++ src/main/java/cloud/tianai/captcha/service/controller/UserLoginController.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.util.DigestUtils;
 
 @RestController
 @RequestMapping("/api")
